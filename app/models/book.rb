0286f0a793545d400788790f187d491c5427d0e7
manish kukreja <manishkukreja@gmail.com>
2012-03-10 00:51:02 +0530

    bookshelf_3

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,6 +3,12 @@ class Book < ActiveRecord::Base
   has_many :tags, :through => :taggings
   
   
+  scope :published, lambda { where('published_at <= ?', Time.now.utc) }
+  scope :unpublished, lambda { where('published_at > ?', Time.now.utc) }
+  scope :tagged, lambda { |tag_id| tag_id ? joins(:taggings).where(:taggings => {:tag_id => tag_id}) : scoped }
+  scope :recent, order('position DESC')
+  
+  
   def self.search(search)
   if search
     find(:all, :conditions => ['name LIKE ?', "%#{search}%"])
@@ -10,6 +16,10 @@ class Book < ActiveRecord::Base
     find(:all)
   end
 end
+
+ def published?
+    published_at <= Time.zone.now
+  end
   
   
 end
