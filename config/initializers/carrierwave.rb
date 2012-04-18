46d75d49c8c0d73dee26c52c28aa9ada8761fb65
manish kukreja <manishkukreja@gmail.com>
2012-04-18 09:39:45 +0530

    bits

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -10,11 +10,11 @@ CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',       # required
     :aws_access_key_id      => 'AKIAJITYSNB4IHHPX4KQ',       # required
-    :aws_secret_access_key  => 'trackkr1',       # required
-    :region                 => 'eu-west-1'  # optional, defaults to 'us-east-1'
+    :aws_secret_access_key  => '1s+Wiy6l5rhLCVxaMO59wQr/DdBWeKp7ZHrXq7sp',       # required
+    :region                 => 'US-Standard'  # optional, defaults to 'us-east-1'
   }
   config.fog_directory  = 'bookshelf_audio'                     # required
-  #config.fog_host       = 'https://assets.example.com'            # optional, defaults to nil
+  config.fog_host       = 'https://www.gmail.com'            # optional, defaults to nil
   config.fog_public     = true                                   # optional, defaults to true
   config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
 end
